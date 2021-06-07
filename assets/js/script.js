37507086334eccc3fb7ab27247acc701890e3aeb
Carolina Reedy <carinvid@gmail.com>
2021-06-07 11:21:20 -0600

    addinng trash

--- assets/js/script.js
+++ assets/js/script.js
@@ -76,23 +76,28 @@ $("#task-form-modal .btn-primary").click(function () {
   }
 });
 
-// remove all tasks
-$("#remove-tasks").on("click", function () {
-  for (var key in tasks) {
-    tasks[key].length = 0;
-    $("#list-" + key).empty();
-  }
-  saveTasks();
-});
+// // remove all tasks
+// $("#remove-tasks").on("click", function () {
+//   for (var key in tasks) {
+//     tasks[key].length = 0;
+//     $("#list-" + key).empty();
+//   }
+//   saveTasks();
+// });
 
 $(".list-group").on("click", "p", function () {
   var text = $(this).text().trim();
+
+  //replace p element with a new text area
   var textInput = $("<textarea>").addClass("form-control").val(text);
   $(this).replaceWith(textInput);
+
+  //auto focus new element
   textInput.trigger("focus");
 });
-// load tasks for the first time
-loadTasks();
+
+// editable field was un-focused
+
 $(".list-group").on("blur", "textarea", function () {
   // get the textarea's current value/text
   var text = $(this).val().trim();
@@ -104,19 +109,18 @@ $(".list-group").on("blur", "textarea", function () {
   var index = $(this).closest(".list-group-item").index();
   tasks[status][index].text = text;
   saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>").addClass("m-1").text(text);
+
+  // replace textarea with p element
+  $(this).replaceWith(taskP);
 });
 
-// recreate p element
-var taskP = $("<p>").addClass("m-1").text(text);
-
-// replace textarea with p element
-$(this).replaceWith(taskP);
-/ due date was clicked
-$(".list-group").on("click", "span", function() {
+// due date was clicked
+$(".list-group").on("click", "span", function () {
   // get current text
-  var date = $(this)
-    .text()
-    .trim();
+  var date = $(this).text().trim();
 
   // create new input element
   var dateInput = $("<input>")
@@ -132,22 +136,15 @@ $(".list-group").on("click", "span", function() {
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("blur", "input[type='text']", function () {
   // get current text
-  var date = $(this)
-    .val()
-    .trim();
+  var date = $(this).val().trim();
 
   // get the parent ul's id attribute
-  var status = $(this)
-    .closest(".list-group")
-    .attr("id")
-    .replace("list-", "");
+  var status = $(this).closest(".list-group").attr("id").replace("list-", "");
 
   // get the task's position in the list of other li elements
-  var index = $(this)
-    .closest(".list-group-item")
-    .index();
+  var index = $(this).closest(".list-group-item").index();
 
   // update task in array and re-save to localstorage
   tasks[status][index].date = date;
@@ -161,3 +158,14 @@ $(".list-group").on("blur", "input[type='text']", function() {
   // replace input with span element
   $(this).replaceWith(taskSpan);
 });
+// remove all tasks
+$("#remove-tasks").on("click", function () {
+  for (var key in tasks) {
+    tasks[key].length = 0;
+    $("#list-" + key).empty();
+  }
+  saveTasks();
+});
+
+// load tasks for the first time
+loadTasks();
